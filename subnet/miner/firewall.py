a95b070d8ec1cda0b65ffb68dfac04091325012e
eclipsevortex <eclipsevortext@hotmail.com>
2024-06-04 16:29:16 -0400

    wip

--- subnet/miner/firewall.py
+++ subnet/miner/firewall.py
@@ -6,7 +6,7 @@ import threading
 import bittensor as bt
 from collections import defaultdict
 from dataclasses import dataclass
-from scapy.all import sniff, TCP, UDP, IP, Raw
+from scapy.all import sniff, TCP, UDP, IP, Raw, Packet
 
 from subnet.miner.iptables import (
     deny_traffic_from_ip,
@@ -72,78 +72,79 @@ class Firewall(threading.Thread):
         super().join()
         bt.logging.debug(f"Firewall stopped")
 
-    def block_ip(self, ip, port, reason):
+    def block_ip(self, ip, port, protocol, reason):
         ip_blocked = next(
-            (x for x in self.ips_blocked if x["ip"] == ip and x["port"] == port), None
+            (x for x in self.ips_blocked if x["ip"] == ip and x["port"] == port and x["protocol"] == protocol), None
         )
         if ip_blocked:
             return
 
         # Update the ip tables
-        deny_traffic_from_ip_and_port(ip, port)
+        deny_traffic_from_ip_and_port(ip, port, protocol)
 
         # Update the block ips
-        ip_blocked = {"ip": ip, "port": port, "reason": reason}
+        ip_blocked = {"ip": ip, "port": port, "protocol": protocol, "reason": reason}
         self.ips_blocked.append(ip_blocked)
 
         # Update the local file
         with open("ips_blocked.txt", "a") as file:
             file.write(json.dumps(ip_blocked) + "\n")
 
-    def unblock_ip(self, ip, port):
+    def unblock_ip(self, ip, port, protocol):
         ip_blocked = next(
-            (x for x in self.ips_blocked if x["ip"] == ip and x["port"] == port), None
+            (x for x in self.ips_blocked if x["ip"] == ip and x["port"] == port and x['protocol'] == protocol), None
         )
         if not ip_blocked:
             return
 
         # Update the ip tables
-        remove_deny_traffic_from_ip_and_port(ip, port)
+        remove_deny_traffic_from_ip_and_port(ip, port, protocol)
 
         # Update the block ips
         self.ips_blocked = [
-            x for x in self.ips_blocked if x["ip"] != ip or x["port"] != port
+            x for x in self.ips_blocked if x["ip"] != ip or x["port"] != port or x["protocol"] != protocol
         ]
 
         # Update the local file
         with open("ips_blocked.txt", "w") as file:
             file.write(json.dumps(self.ips_blocked))
 
-    def detect_dos(self, ip, port, option: FirewallOptions):
+    def detect_dos(self, ip, port, protocol, option: FirewallOptions):
         """
         Detect Denial of Service attack which is an attack from a single source that overwhelms a target with requests,
         """
         current_time = time.time()
 
         self.packet_counts[ip][port] += 1
-        self.packet_timestamps[ip][port].append(current_time)
+        self.packet_timestamps[ip][port][protocol].append(current_time)
 
         recent_packets = [
             t
-            for t in self.packet_timestamps[ip][port]
+            for t in self.packet_timestamps[ip][port][protocol]
             if current_time - t < option.dos_time_window
         ]
-        self.packet_timestamps[ip][port] = recent_packets
+        self.packet_timestamps[ip][port][protocol] = recent_packets
 
         if len(recent_packets) > option.dos_packet_threshold:
             self.block_ip(
                 ip,
                 port,
+                protocol,
                 f"DoS attack detected: {len(recent_packets)} packets in {option.dos_time_window} seconds",
             )
             return True
 
         return False
 
-    def detect_ddos(self, ip, port, option: FirewallOptions):
+    def detect_ddos(self, ip, port, protocol, option: FirewallOptions):
         """
         Detect Distributed Denial of Service which is an attack from multiple sources that overwhelms a target with requests,
         """
         current_time = time.time()
 
-        self.packet_timestamps[ip][port].append(current_time)
+        self.packet_timestamps[ip][port][protocol].append(current_time)
 
-        all_timestamps = [t for ts in self.packet_timestamps.values() for t in ts[port]]
+        all_timestamps = [x for ts in self.packet_timestamps.values() for t in ts[port] for x in t[protocol]]
         recent_timestamps = [
             t for t in all_timestamps if current_time - t < option.ddos_time_window
         ]
@@ -152,49 +153,38 @@ class Firewall(threading.Thread):
             self.block_ip(
                 ip,
                 port,
+                protocol,
                 f"DDoS attack detected: {len(recent_timestamps)} packets in {option.ddos_time_window} seconds",
             )
             return True
 
         return False
 
-    def detect_specific_body(self, packet, port, config):
-        if Raw not in packet:
-            return False
-
-        if config.specific_body_content and Raw in packet:
-            if config.specific_body_content in packet[Raw].load:
-                ip_src = packet[IP].src
-                if False:
-                    bt.logging.warning(
-                        f"Specific content detected in packet from IP {ip_src} on port {port}"
-                    )
-                self.block_ip(
-                    ip_src,
-                    port,
-                    f"Specific content detected in packet: {config.specific_body_content}",
-                )
-                return True
-
-        return False
-
-    def get_rule(self, rules, type, ip, port):
+    def get_rule(self, rules, type, ip, port, protocol):
         filtered_rules = [r for r in rules if r.get("type") == type]
+
+        # Ip/Port rule
         rule = next(
-            (r for r in filtered_rules if r.get("ip") == ip and r.get("port") == port),
+            (r for r in filtered_rules if r.get("ip") == ip and r.get("port") == port and r.get("protocol") == protocol),
             None,
         )
+
+        # Ip rule
         rule = rule or next(
             (r for r in filtered_rules if ip is not None and r.get("ip") == ip), None
         )
+
+        # Port rule
         rule = rule or next(
-            (r for r in filtered_rules if port is not None and r.get("port") == port),
+            (r for r in filtered_rules if port is not None and r.get("port") == port and r.get("protocol") == protocol),
             None,
         )
+
         return rule
 
-    def packet_callback(self, packet):
+    def packet_callback(self, packet: Packet):
         ip_src = packet[IP].src if IP in packet else None
+        protocol = "protocol" if TCP in packet else "udp" if UDP in packet else None
         port_dest = (
             packet[TCP].dport
             if TCP in packet
@@ -210,34 +200,35 @@ class Firewall(threading.Thread):
         ]
 
         # Check if a forward rule exists
-        rule = self.get_rule(rules=rules, type="allow", ip=ip_src, port=port_dest)
+        # rule = self.get_rule(rules=rules, type="allow", ip=ip_src, port=port_dest, protocol=protocol)
 
-        block_packet = rule is None
+        # block_packet = rule is None
+        block_packet = False
 
-        # Check if a block rule exists
-        rule = self.get_rule(rules=rules, type="deny", ip=ip_src, port=port_dest)
-        if rule:
-            self.block_ip(ip_src, port_dest, f"Block ip {ip_src}")
-            return
+        # # Check if a block rule exists
+        # rule = self.get_rule(rules=rules, type="deny", ip=ip_src, port=port_dest, protocol=protocol)
+        # if rule:
+        #     self.block_ip(ip_src, port_dest, protocol, f"Block ip {ip_src}")
+        #     return
 
         # Check if a DoS rule exist
-        rule = self.get_rule(rules=rules, type="detect-dos", ip=ip_src, port=port_dest)
+        rule = self.get_rule(rules=rules, type="detect-dos", ip=ip_src, port=port_dest, protocol=protocol)
         block_packet |= rule is not None and self.detect_dos(
-            ip_src, port_dest, FirewallOptions(rule.get("configuration"))
+            ip_src, port_dest, protocol, FirewallOptions(rule.get("configuration"))
         )
 
         # Check if a DDoS rule exist
-        rule = self.get_rule(rules=rules, type="detect-ddos", ip=ip_src, port=port_dest)
+        rule = self.get_rule(rules=rules, type="detect-ddos", ip=ip_src, port=port_dest, protocol=protocol)
         block_packet |= rule is not None and self.detect_ddos(
-            ip_src, port_dest, FirewallOptions(rule.get("configuration"))
+            ip_src, port_dest, protocol, FirewallOptions(rule.get("configuration"))
         )
 
         if block_packet:
-            self.block_ip(ip_src, port_dest, f"Block ip {ip_src}")
+            self.block_ip(ip_src, port_dest, protocol, f"Block ip {ip_src}")
             return
 
         # Unblock the ip/port
-        self.unblock_ip(ip_src, port_dest)
+        self.unblock_ip(ip_src, port_dest, protocol)
 
     def run(self):
         # Reload the previous ips blocked
@@ -246,29 +237,30 @@ class Firewall(threading.Thread):
             with open("ips_blocked.json", "r") as file:
                 self.ips_blocked = json.load(file) or []
 
-        bt.logging.debug(f"Loading allow/deny rules")
+        bt.logging.debug(f"Applying allow/deny rules")
         for rule in self.rules:
             if rule.get("type") not in ["allow", "deny"]:
                 continue
 
             ip = rule.get("ip")
             port = rule.get("port")
+            protocol = rule.get("protocol")
             type = rule.get("type")
 
             if type == "allow":
                 if ip and port:
-                    allow_traffic_from_ip_and_port(ip, port)
+                    allow_traffic_from_ip_and_port(ip, port, protocol)
                 elif ip:
                     allow_traffic_from_ip(ip)
                 elif port:
-                    allow_traffic_on_port(port)
+                    allow_traffic_on_port(port, protocol)
             else:
                 if ip and port:
-                    deny_traffic_from_ip_and_port(ip, port)
+                    deny_traffic_from_ip_and_port(ip, port, protocol)
                 elif ip:
                     deny_traffic_from_ip(ip)
                 elif port:
-                    deny_traffic_on_port(port)
+                    deny_traffic_on_port(port, protocol)
 
         # Start sniffing with the filter
         sniff(
